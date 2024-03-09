7de6dd128af038d13d496301971bac2549a3438e
KHarshaVardhanC <109226763+KHarshaVardhanC@users.noreply.github.com>
2024-03-09 20:01:58 +0530

    Update LMSLogin.java

--- LMSLogin.java
+++ LMSLogin.java
@@ -3,20 +3,18 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
-public class LMSLogin {
-    public static void main(String[] args) {
+public class LMSLogin 
+{
+    public static void main(String[] args) 
+    {
         WebDriver driver;
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
-
-
+        WebDriverManager.edgedriver().setup();
+        driver= new EdgeDriver();
         driver.get("https://lms.kluniversity.in/login/index.php");
-
         driver.findElement(By.name("username")).sendKeys("2100030247");
         driver.findElement(By.name("password")).sendKeys("Vardhan1234@");
-
         driver.findElement(By.id("loginbtn")).click();
     }
 }
